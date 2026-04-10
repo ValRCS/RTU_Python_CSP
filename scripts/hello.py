fb46351b8307473e760a9ab4e5c03ee785b5b01d
Valdis Saulespurens <valdis.s.coding@gmail.com>
2026-04-10 12:18:22 +0300

    Apr 10 changes

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,2 +1,6 @@
 # komentāri Python sākas ar # simbolu, un tie tiek ignorēti izpildes laikā
-print("Sveika, pasaule! 😊 ")
+print("Sveika, pasaule! 😊 Priekā!")
+x = 10
+print("x vērtība ir:", x)
+print("x + 5 =", x + 5)
+# darbs ir šeit beidzies un x vairs nav pieejams
